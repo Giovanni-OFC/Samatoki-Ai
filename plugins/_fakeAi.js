7ae235ac98f86977a330b698f1d6c63e6d5fd98d
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-21 23:59:21 -0600

    Actualizar _fakeAi.js

--- plugins/_fakeAi.js
+++ plugins/_fakeAi.js
@@ -14,13 +14,13 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
           "groupJid": "6285240750713-1610340626@g.us",
           "inviteCode": "mememteeeekkeke",
           "groupName": "P",
-          "caption": "Hello, I'm Ai Hoshino",
+          "caption": "Hello, I'm Samatoki Ai",
           "jpegThumbnail": await (await fetch(pp)).buffer()
         }
       }
     }
     
-    conn.sendMessage(m.chat, { text: 'Hola soy Ai Hoshino, ¿Cómo puedo ayudarte?' }, { quoted: anu })
+    conn.sendMessage(m.chat, { text: 'Hola soy Samatoki Ai, ¿Cómo puedo ayudarte?' }, { quoted: anu })
   } catch (error) {
     conn.sendMessage(m.chat, 'Hola soy Ai Hoshino, ¿Cómo puedo ayudarte?', 'conversation', { quoted: m })
   }
