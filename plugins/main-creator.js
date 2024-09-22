eb9d2a824e03da91e9a4c87913c93a08ad044624
Giovanni-OFC <jostingaleano10@gmail.com>
2024-09-22 00:09:53 -0600

    Actualizar main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Giovanni-OFC;;\nFN:おDanịel.xyz⁩\nORG:Giovanni-OFC\nTITLE:\nitem1.TEL;waid=51902855352:51902855352\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おDanịel.xyz⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Giovanni-OFC;;\nFN:Giovanni-OFC\nORG:Giovanni-OFC\nTITLE:\nitem1.TEL;waid=51902855352:51902855352\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おDanịel.xyz⁩\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Giovanni-OFC', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
